e0e8339b74b7502d2bf676c87140bcae5a0975e8
Jared Roesch <roeschinc@gmail.com>
2012-05-06 21:03:30 -0700

    Better Version that grabs all the track information, and transforms it into a Hash

--- Client.rb
+++ Client.rb
@@ -1,27 +1,26 @@
 require 'rubygems'
 require 'nokogiri'
 
-class Array 
-  def dict_zip(xs)
-    dict = {}
-    n = length > xs.length ? length : length.xs
-    (0..n).each do |i|
-      dict[self[i]] = xs[i]
-    end
-    dict
-  end
-end
-
 file_name = "/Users/roeschinc/Music/iTunes/iTunes Music Library.xml"
 
 library_file = File.open(file_name, 'r')
 
-library = Nokogiri::XML(library_file.read)
+library_xml = Nokogiri::XML(library_file.read)
 
-songs = library.xpath("//dict").map do |dict|
-  keys = dict.xpath("//key").map { |i| i.content }
-  values = dict.xpath("//string").map { |i| i.content }
-  keys.dict_zip(values)
+# Fragile Parsing Method Currently works well for our purposes
+# Needs some clean up also
+songs = library_xml.css("dict > dict > dict").map do |dict|
+  nodes = dict.css("*").map { |i| i.content.to_s } 
+  song = {}; i = 0;
+  # Check for now to make sure each key is paired with a value
+  throw "Not Even." unless nodes.length % 2 == 0
+  while i < nodes.length
+    song[nodes[i]] = nodes[i+1]
+    i += 2
+  end
+  song
 end
 
-puts songs[0]
+class iTunesParser
+  
+end
