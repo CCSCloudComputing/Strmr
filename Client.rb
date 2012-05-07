df12c745c71d3a0aa0a2dcc5bdbad71944954ba1
Jared Roesch <roeschinc@gmail.com>
2012-05-06 21:13:29 -0700

    Cleaned up the parser into a clean interface for use, with more to come. Usage of Parser to present a song list

--- Client.rb
+++ Client.rb
@@ -1,26 +1,44 @@
 require 'rubygems'
 require 'nokogiri'
 
+class ITunesParser
+  def initialize(filename)
+    @filename = filename
+    @result = nil
+  end
+  
+  def parse
+    library_file = File.open(@filename, 'r')
+    library_xml = Nokogiri::XML(library_file.read)
+    
+    # Fragile Parsing Method Currently works well for our purposes
+    # Needs some clean up also
+    songs = library_xml.css("dict > dict > dict").map do |dict|
+      nodes = dict.css("*").map { |i| i.content.to_s } 
+      song = {}; i = 0;
+      # Check for now to make sure each key is paired with a value
+      throw "Not Even." unless nodes.length % 2 == 0
+      while i < nodes.length
+        song[nodes[i]] = nodes[i+1]
+        i += 2
+      end
+      song
+    end
+    @result = songs
+  end
+  
+  
+  def result
+    @result
+  end
+end
+
 file_name = "/Users/roeschinc/Music/iTunes/iTunes Music Library.xml"
 
-library_file = File.open(file_name, 'r')
+itunes = ITunesParser.new(file_name)
 
-library_xml = Nokogiri::XML(library_file.read)
+songs = itunes.parse
 
-# Fragile Parsing Method Currently works well for our purposes
-# Needs some clean up also
-songs = library_xml.css("dict > dict > dict").map do |dict|
-  nodes = dict.css("*").map { |i| i.content.to_s } 
-  song = {}; i = 0;
-  # Check for now to make sure each key is paired with a value
-  throw "Not Even." unless nodes.length % 2 == 0
-  while i < nodes.length
-    song[nodes[i]] = nodes[i+1]
-    i += 2
-  end
-  song
-end
+song_list = songs.map { |song| song["Name"] }
 
-class iTunesParser
-  
-end
+puts song_list
